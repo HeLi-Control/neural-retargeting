64849e9cb730d7858ea00bd8a76b75f82c7984ae
HeLi-Control <1939821708@qq.com>
2024-01-16 23:33:05 +0800

    Added the debug messages but have not found problems yet.

--- inference.py
+++ inference.py
@@ -32,121 +32,172 @@ cfg.freeze()
 # Device setting
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
+class Inference:
+    def __init__(self) -> None:
+        self.load_data()
+        self.inferenced_data = {
+            "l_arm": [],
+            "r_arm": [],
+            "l_hand": [],
+            "r_hand": [],
+        }
+
+    def init_model(self, model_name: str):
+        # Create model
+        self.model = getattr(model, model_name)().to(device)
+
+    def load_data(self):
+        # Load data
+        pre_transform = transforms.Compose([Normalize()])
+        self.test_set = getattr(dataset, cfg.DATASET.TEST.SOURCE_NAME)(
+            root=cfg.DATASET.TEST.SOURCE_PATH, pre_transform=pre_transform
+        )
+        self.test_loader = DataListLoader(
+            self.test_set,
+            batch_size=cfg.HYPER.BATCH_SIZE,
+            shuffle=False,
+            num_workers=8,
+            pin_memory=True,
+        )
+        self.test_target = sorted(
+            [
+                target
+                for target in getattr(dataset, cfg.DATASET.TEST.TARGET_NAME)(
+                    root=cfg.DATASET.TEST.TARGET_PATH
+                )
+            ],
+            key=lambda target: target.skeleton_type,
+        )
+
+    def load_trained_model(self, model_file: str):
+        self.model.load_state_dict(torch.load(model_file))
+
+    def inference_frame(self, data_list):
+        target_list = [self.test_target[0] for _ in data_list]
+        # forward
+        _, arm_data, hand_data = self.model(
+            Batch.from_data_list(data_list).to(device),
+            Batch.from_data_list(target_list).to(device),
+        )
+        return arm_data, hand_data
+
+    def save_inferenced_frame_data(self, arm_data, hand_data):
+        self.inferenced_data["l_arm"].append(
+            arm_data.arm_ang.squeeze().tolist()[: int(arm_data.arm_ang.size(0) / 2)]
+        )
+        self.inferenced_data["r_arm"].append(
+            arm_data.arm_ang.squeeze().tolist()[int(arm_data.arm_ang.size(0) / 2) :]
+        )
+        self.inferenced_data["l_hand"].append(
+            hand_data.l_hand_ang.squeeze().tolist()[1:]
+        )
+        self.inferenced_data["r_hand"].append(
+            hand_data.r_hand_ang.squeeze().tolist()[1:]
+        )
+
+    def save_inferenced_all_data(self, save_path: str):
+        h5_file = h5py.File(save_path, "w")
+        if h5_file is not None:
+            h5_file["l_arm"] = self.inferenced_data["l_arm"]
+            h5_file["r_arm"] = self.inferenced_data["r_arm"]
+            h5_file["l_hand"] = self.inferenced_data["l_hand"]
+            h5_file["r_hand"] = self.inferenced_data["r_hand"]
+        h5_file.close()
+
+
+def search_checkpoints(checkpoints_path: str):
+    # Search checkpoints
+    paths = os.walk(checkpoints_path)
+    inference_model_files = []
+    for path, _, file_list in paths:
+        for file in file_list:
+            if file.endswith(".pth"):
+                inference_model_files.append(
+                    os.path.join(path, file).replace("\\", "/")
+                )
+    return inference_model_files
+
+
+def save_demonstrate_actions(save_file_name: str, test_loader):
+    human_demonstrate_data = {
+        "l_arm": [],
+        "r_arm": [],
+        "l_hand": [],
+        "r_hand": [],
+        "ee_ori": [],
+    }
+    for _, data_list in enumerate(test_loader):
+        human_demonstrate_data["l_arm"].append(data_list[0].pos[:3].tolist())
+        human_demonstrate_data["r_arm"].append(data_list[0].pos[3:].tolist())
+        human_demonstrate_data["l_hand"].append(data_list[0].l_hand_pos.tolist())
+        human_demonstrate_data["r_hand"].append(data_list[0].r_hand_pos.tolist())
+        human_demonstrate_data["ee_ori"].append(
+            [data_list[0].q[2].tolist(), data_list[0].q[5].tolist()]
+        )
+    h5_file = h5py.File(save_file_name, "w")
+    h5_file["l_arm"] = human_demonstrate_data["l_arm"]
+    h5_file["r_arm"] = human_demonstrate_data["r_arm"]
+    h5_file["l_hand"] = human_demonstrate_data["l_hand"]
+    h5_file["r_hand"] = human_demonstrate_data["r_hand"]
+    h5_file["ee_ori"] = human_demonstrate_data["ee_ori"]
+    h5_file.close()
+
+
 if __name__ == "__main__":
-    # Load data
-    pre_transform = transforms.Compose([Normalize()])
-    # inference all
-    print("Inference all")
-    test_set = getattr(dataset, cfg.DATASET.TEST.SOURCE_NAME)(
-        root=cfg.DATASET.TEST.SOURCE_PATH, pre_transform=pre_transform
-    )
-    test_loader = DataListLoader(
-        test_set,
-        batch_size=cfg.HYPER.BATCH_SIZE,
-        shuffle=False,
-        num_workers=8,
-        pin_memory=True,
-    )
-    test_target = sorted(
-        [
-            target
-            for target in getattr(dataset, cfg.DATASET.TEST.TARGET_NAME)(
-                root=cfg.DATASET.TEST.TARGET_PATH
-            )
-        ],
-        key=lambda target: target.skeleton_type,
-    )
-
-    # Create model
-    model = getattr(model, cfg.MODEL.NAME)().to(device)
-
-    # Load checkpoints
-    if cfg.MODEL.CHECKPOINT is not None:
-        checkpoints_path = cfg.MODEL.CHECKPOINT
-        paths = os.walk(checkpoints_path)
-        inference_model_files = []
-        for path, _, file_list in paths:
-            for file in file_list:
-                if file.endswith(".pth"):
-                    inference_model_files.append(
-                        os.path.join(path, file).replace("\\", "/")
-                    )
-        if len(inference_model_files) == 0:
-            print("No models to inference.")
-            exit(1)
-    else:
-        print("Checkpoint path unspecified.")
-        exit(1)
-
-    inferenced_data = (
-        {"l_arm": [], "r_arm": [], "l_hand": [], "r_hand": []}
-        if (cfg.INFERENCE.H5.BOOL or cfg.INFERENCE.PYBULLET.BOOL)
-        else None
-    )
+    inference = Inference()
     create_folder(cfg.INFERENCE.H5.PATH)
 
+    # Save source data
+    if cfg.INFERENCE.RUN.HUMAN_DEMONSTRATE:
+        print("Saving source data.")
+        save_demonstrate_actions(
+            cfg.INFERENCE.H5.PATH + "humanDemonstrate.h5", inference.test_loader
+        )
+
     # Inference
-    for model_file in inference_model_files:
-        print("Loading model:", model_file)
-        model.load_state_dict(torch.load(model_file))
-        print("Model evaluating...")
-        model.eval()
-        print("Inferencing...")
+    if cfg.INFERENCE.RUN.INFERENCE:
         # Start simulation
         if cfg.INFERENCE.PYBULLET.BOOL:
             from simulation import YuMi_Simulation
-        yumi_sim = YuMi_Simulation() if cfg.INFERENCE.PYBULLET.BOOL else None
-        for batch_idx, data_list in tqdm(
-            enumerate(test_loader), total=len(test_loader), leave=False
-        ):
-            for target_idx, target in enumerate(test_target):
-                target_list = [target for _ in data_list]
-                # forward
-                _, arm_data, hand_data = model(
-                    Batch.from_data_list(data_list).to(device),
-                    Batch.from_data_list(target_list).to(device),
-                )
+        yumi_sim = (
+            YuMi_Simulation(disp_gui=True, debug_message=False)
+            if cfg.INFERENCE.PYBULLET.BOOL
+            else None
+        )
+        print("Inferencing.")
+        inference.init_model()
+        inference_model_files = search_checkpoints(cfg.MODEL.CHECKPOINT)
+        for model_file in inference_model_files:
+            # load model
+            inference.load_trained_model(model_file)
+            print("Loaded model:", model_file)
+            # Inference
+            for _, data_list in tqdm(
+                enumerate(inference.test_loader),
+                total=len(inference.test_loader),
+                leave=False,
+            ):
+                # Inference a frame
+                arm_data, hand_data = inference.inference_frame(data_list)
                 # Save data
-                if inferenced_data is not None:
-                    inferenced_data["l_arm"].append(
-                        arm_data.arm_ang.squeeze().tolist()[
-                            : int(arm_data.arm_ang.size(0) / 2)
-                        ]
-                    )
-                    inferenced_data["r_arm"].append(
-                        arm_data.arm_ang.squeeze().tolist()[
-                            int(arm_data.arm_ang.size(0) / 2) :
-                        ]
-                    )
-                    inferenced_data["l_hand"].append(
-                        hand_data.l_hand_ang.squeeze().tolist()[1:]
-                    )
-                    inferenced_data["r_hand"].append(
-                        hand_data.r_hand_ang.squeeze().tolist()[1:]
-                    )
-                    # Step the simulation
-                    data_frame = (
-                        inferenced_data["l_arm"][-1]
-                        + inferenced_data["l_hand"][-1]
-                        + inferenced_data["r_arm"][-1]
-                        + inferenced_data["r_hand"][-1]
-                    )
+                inference.save_inferenced_frame_data(arm_data, hand_data)
+                # Step the simulation
                 if cfg.INFERENCE.PYBULLET.BOOL:
-                    yumi_sim.step_simulation(data_frame)
+                    data_frame = (
+                        inference.inferenced_data["l_arm"][-1]
+                        + inference.inferenced_data["l_hand"][-1]
+                        + inference.inferenced_data["r_arm"][-1]
+                        + inference.inferenced_data["r_hand"][-1]
+                    )
+                    yumi_sim.step_simulation(given_joints_angles=data_frame)
+
         if cfg.INFERENCE.PYBULLET.BOOL:
             # Stop the simulation
             yumi_sim.close_simulation()
             del yumi_sim
 
         if cfg.INFERENCE.H5.BOOL:
-            # save data
-            print("Data saving...")
-            # New data file
-            save_file_name = os.path.basename(model_file)[:-4] + ".h5"
-            h5_file = h5py.File(cfg.INFERENCE.H5.PATH + save_file_name, "w")
-            if h5_file is not None:
-                h5_file["l_arm"] = inferenced_data["l_arm"]
-                h5_file["r_arm"] = inferenced_data["r_arm"]
-                h5_file["l_hand"] = inferenced_data["l_hand"]
-                h5_file["r_hand"] = inferenced_data["r_hand"]
-            h5_file.close()
+            inference.save_inferenced_all_data(
+                cfg.INFERENCE.H5.PATH + os.path.basename(model_file)[:-4] + ".h5"
+            )
--- simulation.py
+++ simulation.py
@@ -4,10 +4,166 @@ import h5py
 import os
 from models.kinematics import ForwardKinematicsURDF
 import torch
+from scipy.spatial.transform import Rotation
+from models.loss import Loss, CollisionLoss, calculate_all_loss
+import torch.nn as nn
+
+import dataset
+
+
+class Display_Debug_Msg:
+    def __init__(self, debug=True, inferenced_file=None, demonstrate_file=None) -> None:
+        self.debug_message = debug
+        if not debug:
+            return
+        self.hand_fk = ForwardKinematicsURDF()
+        self.targets = sorted(
+            [
+                target
+                for target in getattr(dataset, "YumiAll")(root="./data/target/yumi-all")
+            ],
+            key=lambda target: target.skeleton_type,
+        )[0]
+        self.human_demonstrate = (
+            demonstrate_file if demonstrate_file is not None else None
+        )
+        self.inferenced_file = inferenced_file if inferenced_file is not None else None
+        self.loss_criterion = Loss(
+            ee=nn.MSELoss(),
+            vec=nn.MSELoss(),
+            col=CollisionLoss(0.15),
+            lim=None,
+            ori=nn.MSELoss(),
+            fin=nn.MSELoss(),
+            reg=None,
+        )
+
+    def __calc_hand_forward_kinematics(self, l_arm, r_arm):
+        _, _, global_pos = self.hand_fk(
+            torch.cat([l_arm, r_arm]),
+            self.targets.parent,
+            self.targets.offset,
+            1,
+        )
+        global_pos[:, 2] = global_pos[:, 2] + 0.51492
+        return global_pos
+
+    def disp_forward_kinematics(self, index=None, l_arm=None, r_arm=None):
+        # Plot forward kinematics
+        if self.inferenced_file is not None:
+            global_pos = self.__calc_hand_forward_kinematics(
+                torch.tensor(self.inferenced_file["l_arm"][index]),
+                torch.tensor(self.inferenced_file["r_arm"][index]),
+            )
+        else:
+            global_pos = self.__calc_hand_forward_kinematics(
+                torch.tensor(l_arm),
+                torch.tensor(r_arm),
+            )
+        pybullet.addUserDebugPoints(
+            pointPositions=global_pos.tolist(),
+            pointColorsRGB=[[1, 0, 0] for _ in global_pos.tolist()],
+            pointSize=10,
+            lifeTime=0.95,
+        )
+
+    def disp_demonstrate_arm(self, index=None, l_arm=None, r_arm=None):
+        if index is not None:
+            froms = (
+                self.human_demonstrate["l_arm"][index][:2] + [0, 1, 1]
+            ).tolist() + (
+                self.human_demonstrate["r_arm"][index][:2] + [0, 1, 1]
+            ).tolist()
+            tos = (self.human_demonstrate["l_arm"][index][1:] + [0, 1, 1]).tolist() + (
+                self.human_demonstrate["r_arm"][index][1:] + [0, 1, 1]
+            ).tolist()
+        else:
+            froms = (l_arm[:2] + [0, 1, 1]).tolist() + (r_arm[:2] + [0, 1, 1]).tolist()
+            tos = (l_arm[1:] + [0, 1, 1]).tolist() + (r_arm[1:] + [0, 1, 1]).tolist()
+        for f, t in zip(froms, tos):
+            pybullet.addUserDebugLine(
+                lineFromXYZ=f,
+                lineToXYZ=t,
+                lineColorRGB=[1, 0, 1],
+                lineWidth=4,
+                lifeTime=0.95,
+            )
+
+    def disp_end_effector_oritation(
+        self, index=None, ee_matrix_l=None, ee_matrix_r=None
+    ):
+        if self.human_demonstrate is not None:
+            quaternion1 = Rotation.from_quat(self.human_demonstrate["ee_ori"][index][0])
+            quaternion2 = Rotation.from_quat(self.human_demonstrate["ee_ori"][index][1])
+            ee_matrix_l = quaternion1.as_matrix().tolist()
+            ee_matrix_r = quaternion2.as_matrix().tolist()
+        pybullet.addUserDebugLine(
+            lineFromXYZ=[-0.5, 0, 1],
+            lineToXYZ=[
+                i + j * 0.1
+                for i, j in zip(
+                    [-0.5, 0, 1],
+                    [ee_matrix_l[0][0], ee_matrix_l[1][0], ee_matrix_l[2][0]],
+                )
+            ],
+            lineColorRGB=[0, 0, 1],
+            lineWidth=4,
+            lifeTime=0.95,
+        )
+        pybullet.addUserDebugLine(
+            lineFromXYZ=[0.5, 0, 1],
+            lineToXYZ=[
+                i + j * 0.1
+                for i, j in zip(
+                    [0.5, 0, 1],
+                    [ee_matrix_r[0][0], ee_matrix_r[1][0], ee_matrix_r[2][0]],
+                )
+            ],
+            lineColorRGB=[0, 0, 1],
+            lineWidth=4,
+            lifeTime=0.95,
+        )
+
+    def disp_loss_message(self):
+        # _, losses = calculate_all_loss(
+        #     data_list,
+        #     target_list,
+        #     self.loss_criterion,
+        #     None,
+        #     arm_data,
+        #     hand_data,
+        #     None,
+        #     model_params.loss_gain,
+        # )
+        pass
+
+    def disp(
+        self,
+        index=None,
+        l_arm_ang=None,
+        r_arm_ang=None,
+        l_arm_demonstrate=None,
+        r_arm_demonstrate=None,
+        ee_matrix_l=None,
+        ee_matrix_r=None,
+    ):
+        if not self.debug_message:
+            return
+        self.disp_forward_kinematics(index, l_arm_ang, r_arm_ang)
+        self.disp_demonstrate_arm(index, l_arm_demonstrate, r_arm_demonstrate)
+        self.disp_end_effector_oritation(index, ee_matrix_l, ee_matrix_r)
+        self.disp_loss_message(index, ee_matrix_l, ee_matrix_r)
 
 
 class YuMi_Simulation:
-    def __init__(self, disp_gui=True):
+    def __init__(
+        self,
+        disp_gui=True,
+        debug_message=True,
+        inferenced_file=None,
+        demonstrate_file=None,
+    ):
+        self.inferenced_file = inferenced_file if inferenced_file is not None else None
         # Connect the client
         self.client = (
             pybullet.connect(pybullet.GUI)
@@ -27,10 +183,10 @@ class YuMi_Simulation:
         )
         # Set camera
         pybullet.resetDebugVisualizerCamera(
-            cameraDistance=1.8,
+            cameraDistance=1.6,
             cameraYaw=85,
-            cameraPitch=-35,
-            cameraTargetPosition=[0, 0, 0],
+            cameraPitch=-20,
+            cameraTargetPosition=[0, 0, 0.5],
         )
         # Get joints available
         self.available_joints_indices = [
@@ -39,34 +195,52 @@ class YuMi_Simulation:
             if pybullet.getJointInfo(self.robot_id, i)[2] != pybullet.JOINT_FIXED
         ]
         # Set default color
-        self.rgba_color = pybullet.getVisualShapeData(self.robot_id)[
+        self.default_color = pybullet.getVisualShapeData(self.robot_id)[
             self.available_joints_indices[0]
         ][7]
         self.red_color = [1, 0, 0, 1]
+        # Debug messages
+        self.debug_msg_disp = Display_Debug_Msg(
+            debug=debug_message,
+            inferenced_file=self.inferenced_file,
+            demonstrate_file=demonstrate_file,
+        )
 
     def close_simulation(self):
         pybullet.disconnect(self.client)
+        if self.inferenced_file is not None:
+            self.inferenced_file.close()
 
-    def step_simulation(self, target_positions):
-        # Control joints
+    def step_simulation(self, index=None, given_joints_angles=None):
+        if index is not None:
+            # right arm, right arm, left arm, left hand
+            data_frame = (
+                self.inferenced_file["r_arm"][index].tolist()
+                + self.inferenced_file["r_hand"][index].tolist()
+                + self.inferenced_file["l_arm"][index].tolist()
+                + self.inferenced_file["l_hand"][index].tolist()
+            )
+            # Control joints
+            pybullet.setJointMotorControlArray(
+                bodyUniqueId=self.robot_id,
+                jointIndices=self.available_joints_indices,
+                controlMode=pybullet.POSITION_CONTROL,
+                targetPositions=data_frame,
+            )
+        elif given_joints_angles is not None:
+            # Control joints
+            pybullet.stepSimulation()
+            pybullet.setJointMotorControlArray(
+                bodyUniqueId=self.robot_id,
+                jointIndices=self.available_joints_indices,
+                controlMode=pybullet.POSITION_CONTROL,
+                targetPositions=given_joints_angles,
+            )
         pybullet.stepSimulation()
-        pybullet.setJointMotorControlArray(
-            bodyUniqueId=self.robot_id,
-            jointIndices=self.available_joints_indices,
-            controlMode=pybullet.POSITION_CONTROL,
-            targetPositions=target_positions,
-        )
         # Collision check
         self.collision_check()
-        for joint in self.available_joints_indices:
-            if (
-                ("Link51" in str(self.all_joints_names[joint])[2:])
-                or ("Link52" in str(self.all_joints_names[joint])[2:])
-                or ("Link53" in str(self.all_joints_names[joint])[2:])
-            ):
-                pybullet.changeVisualShape(
-                    self.robot_id, joint, rgbaColor=self.red_color
-                )
+        # Display debug messages
+        self.debug_msg_disp.disp(index)
 
     def collision_check(self):
         for joint in self.available_joints_indices:
@@ -84,8 +258,8 @@ class YuMi_Simulation:
                 ):
                     print(
                         "Collision Occurred in {} & {}!!!".format(
-                            self.all_joints_names[contact[3]][2:],
-                            self.all_joints_names[contact[4]][2:],
+                            str(self.all_joints_names[contact[3]])[2:],
+                            str(self.all_joints_names[contact[4]])[2:],
                         )
                     )
                     pybullet.changeVisualShape(
@@ -96,7 +270,7 @@ class YuMi_Simulation:
                     )
             else:
                 pybullet.changeVisualShape(
-                    self.robot_id, joint, rgbaColor=self.rgba_color
+                    self.robot_id, joint, rgbaColor=self.default_color
                 )
 
     @property
@@ -117,35 +291,32 @@ class YuMi_Simulation:
             for i in range(pybullet.getNumJoints(self.robot_id))
         ]
 
+    @property
+    def data_frame_num(self):
+        return (
+            len(list(self.inferenced_file["l_hand"]))
+            if self.inferenced_file is not None
+            else 0
+        )
 
-if __name__ == "__main__":
-    # Initialize the simulation
-    yumi = YuMi_Simulation()
-    # Read inferenced data
+
+def search_inferenced_files(path_name) -> list:
     h5_files = []
-    for path, _, file_list in os.walk("saved/inferenced"):
+    for path, _, file_list in os.walk(path_name):
         for file in file_list:
             if file.endswith(".h5"):
                 h5_files.append(os.path.join(path, file).replace("\\", "/"))
-    h5_file = h5py.File(h5_files[0], "r")  # Read only the first file
+    return h5_files
+
+
+if __name__ == "__main__":
+    # Initialize the simulation
+    yumi = YuMi_Simulation(
+        inferenced_file=h5py.File(search_inferenced_files("saved/inferenced")[0], "r"),
+        demonstrate_file=h5py.File("saved/inferenced/humanDemonstrate.h5", "r"),
+    )
     # Display all frames
-    for index in range(len(list(h5_file["l_hand"]))):
-        # right arm, right arm, left arm, left hand
-        data_frame = (
-            list(h5_file["r_arm"])[index].tolist()
-            + list(h5_file["r_hand"])[index].tolist()
-            + list(h5_file["l_arm"])[index].tolist()
-            + list(h5_file["l_hand"])[index].tolist()
-        )
-        yumi.step_simulation(data_frame)
-        # # Reconstruct data in network
-        # kinematics_data = torch.tensor(
-        #     list(h5_file["l_arm"])[index].tolist()
-        #     + list(h5_file["r_arm"])[index].tolist()
-        #     + list(h5_file["l_hand"])[index].tolist()
-        #     + list(h5_file["r_hand"])[index].tolist()
-        # )
-        # _, _, global_pos = ForwardKinematicsURDF(kinematics_data.unsqueeze(1), 1)
-    print(yumi.available_joints_names)
+    for index in range(yumi.data_frame_num):
+        yumi.step_simulation(index)
     # Stop the simulation
     yumi.close_simulation()
